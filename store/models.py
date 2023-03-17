6f1bbe1cb9026e92863f4ac062bbb41e7632228f
a-burlakov <fln@mail.ru>
2023-03-17 15:31:50 +0500

    learn: ordering and searching via django-filter

--- store/models.py
+++ store/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField("Название", max_length=255)
     price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    author_name = models.CharField("Автор", max_length=255)
 
     class Meta:
         verbose_name = "Книга"
         verbose_name_plural = "Книги"
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return f"{self.name} by {self.author_name} ({self.price} rub.)"
 
     # def save(self):
     #     pass
