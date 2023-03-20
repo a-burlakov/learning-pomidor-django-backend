91ad916a762c82260cc25d5a53edfe135fc9342f
a-burlakov <fln@mail.ru>
2023-03-20 16:57:19 +0500

    lesson: permissions on "is_staff"

--- store/views.py
+++ store/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
-from store.permissions import IsOwnerOrReadOnly
+from store.permissions import IsOwnerOrStaffOrReadOnly
 from store.serializers import BooksSerializer
 
 
@@ -14,7 +13,7 @@ class BookViewSet(ModelViewSet):
     serializer_class = BooksSerializer
 
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filterset_fields = ["price"]
     search_fields = ["name", "author_name"]
     ordering_fields = ["price", "author_name", "name"]
